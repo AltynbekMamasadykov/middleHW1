9c15e0d9a0ee880715325bf5bb1aaf4fc03aeb3e
Asanbai Ortosaev <asanbaiortosaev@gmail.com>
2022-04-05 02:38:08 +0600

    first level complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,30 @@
 package com.company;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
-
-        Dice.printDice(Dice.rollTheDice());
+        //------------------------------ Level 1 ---------------------------
+        System.out.println("---      Start game      ---");
+        System.out.println("Predict amount of points (2..12)");
+        Scanner scanner = new Scanner(System.in);
+        int userPrediction = scanner.nextInt();
+        System.out.println("User rolls the dices ...");
+        int firstNumber = Dice.rollTheDice();
+        int secondNumber = Dice.rollTheDice();
+        int sum = firstNumber+secondNumber;
+        Dice.printDice(firstNumber);
+        Dice.printDice(secondNumber);
+        System.out.println("On the dice fell "+sum+" points");
+        int result = (sum)-Math.abs(sum-userPrediction)*2;
+        System.out.println("Result is "+sum+"-abs("+sum+"-"+userPrediction+")*2 = "+result);
+        if(result>0){
+            System.out.println("User wins!");
+        }else{
+            System.out.println("User loses!");
+        }
 
 
 
